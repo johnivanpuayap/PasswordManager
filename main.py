d2227888a3dee6f75380c942502b9f9ddb7876eb
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-26 20:04:54 +0800

    Update Exception Handling

--- main.py
+++ main.py
@@ -57,11 +57,11 @@ def add_password():
             try:
                 with open('data.json', mode='r') as data_file:
                     data = json.load(data_file)
-                    data.update(new_data)
             except FileNotFoundError:
                 with open('data.json', mode='w') as data_file:
                     json.dump(new_data, data_file, indent=4)
             else:
+                data.update(new_data)
                 with open('data.json', mode='w') as data_file:
                     json.dump(data, data_file, indent=4)
 
